dffa5eec3fab42234cec6e301f718c5dc2b0b5d4
avhijay <parmaravhijay5049@gmail.com>
2026-08-22 23:34:40 +0530

    Implemented Outbox pattern for Kafka producer

---
+++ payment-service/src/main/java/com/smartremind/payment_service/entity/OutboxData.java
@@ -0,0 +1,48 @@
+package com.smartremind.payment_service.entity;
+
+
+import com.smartremind.payment_service.enums.SubscriptionStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+
+import java.time.Instant;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class OutboxData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    @Setter(AccessLevel.NONE)
+    private Long id;
+
+    @Column(name = "user_name" , nullable = false , length = 250)
+    private String userName;
+
+    @Column(name = "unique_id" , nullable = false , unique = true)
+    private String uniqueId;
+
+    @Column(name = "subscription_status" , nullable = false )
+    @Enumerated(EnumType.STRING)
+    private SubscriptionStatus subscriptionStatus;
+
+    @Column(name = "subscription_plan_id" , nullable = false)
+    private Long subscriptionId;
+
+    @Column(name = "expires_at" , nullable = false)
+    private Instant expiresAt;
+
+    @Column(name = "published" , columnDefinition = "FALSE")
+    private  boolean published;
+
+
+
+
+}
