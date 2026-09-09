bc538cc94101b2e157cfe757c86aa5359b8424c3
avhijay <parmaravhijay5049@gmail.com>
2026-09-09 22:42:29 +0530

    Implemented Rajor Pay Test Api skeleton with payment flow diagram

--- payment-service/src/main/java/com/smartremind/payment_service/dto/provider/PaymentProviderResponseDTO.java
+++ payment-service/src/main/java/com/smartremind/payment_service/dto/provider/PaymentProviderResponseDTO.java
@@ -1,11 +1,21 @@
 package com.smartremind.payment_service.dto.provider;
 
+import com.smartremind.payment_service.enums.Currency;
 import com.smartremind.payment_service.enums.PaymentStatus;
 
+import java.math.BigDecimal;
+
 public record PaymentProviderResponseDTO(
-        String providerTransactionId,
-        String paymentId,
-        PaymentStatus status
+
+        //receipt
+        String paymentId ,
+        BigDecimal amountDue ,
+        String providerOrderId,
+        String status
+
+
+
+
 
 ) {
 }
---
+++ payment-service/src/main/java/com/smartremind/payment_service/payment_provider/RazorPayPaymentProvider.java
@@ -0,0 +1,59 @@
+package com.smartremind.payment_service.payment_provider;
+
+import com.smartremind.payment_service.dto.provider.PaymentProviderRequestDto;
+import com.smartremind.payment_service.dto.provider.PaymentProviderResponseDTO;
+import com.smartremind.payment_service.dto.razorpay.RazorPayRequestDto;
+import com.smartremind.payment_service.dto.razorpay.RazorPayResponseDto;
+import com.smartremind.payment_service.enums.PaymentStatus;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public  class RazorPayPaymentProvider implements PaymentProvider{
+
+
+    @Override
+    public PaymentProviderResponseDTO createOrder(PaymentProviderRequestDto paymentProviderRequestDto) {
+        return null;
+    }
+
+
+
+
+
+
+
+
+
+
+    // receipt is equivalent yo payment Id
+
+    private RazorPayRequestDto mapToRazorPayRequest(PaymentProviderRequestDto providerRequest){
+
+        long amount = providerRequest.amount().movePointRight(2).longValueExact();
+
+        RazorPayRequestDto razorPayRequest = new RazorPayRequestDto(amount,providerRequest.currency(), providerRequest.paymentId());
+        return razorPayRequest;
+
+
+    }
+
+    private PaymentProviderResponseDTO mapToPaymentProviderResponse(RazorPayResponseDto razorPayResponseDto){
+
+        BigDecimal amountDue = BigDecimal.valueOf(razorPayResponseDto.amount()).movePointLeft(2);
+
+
+        PaymentProviderResponseDTO providerResponse =  new
+                PaymentProviderResponseDTO
+                (razorPayResponseDto.receipt(),amountDue, razorPayResponseDto.id(), razorPayResponseDto.status());
+
+        return providerResponse;
+
+
+
+    }
+
+
+
+
+}
